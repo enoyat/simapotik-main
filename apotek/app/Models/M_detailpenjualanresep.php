3910471c8594458a2d138f72a7041b1f3752504d
enoyat <50427196+enoyat@users.noreply.github.com>
2025-07-15 00:38:54 +0700

    1

--- apotek/app/Models/M_detailpenjualanresep.php
+++ apotek/app/Models/M_detailpenjualanresep.php
@@ -1,20 +1,19 @@
 <?php
-
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class M_detailpenjualan extends Model
+class M_detailpenjualanresep extends Model
 {
 
     use HasFactory;
-    protected $table = 'detailpenjualan';
+    protected $table = 'detailpenjualanresep';
 
     #kalau kolom primary keynya bernama id, maka baris dibawah ini boleh diisi, dan boleh juga tidak buat
     protected $primaryKey = 'id';
-    protected $guarded = [];
-    public $timestamps = false;
+    protected $guarded    = [];
+    public $timestamps    = false;
     public function get_penjualan()
     {
         return $this->belongsTo(M_penjualan::class, 'idpenjualan', 'id');
